7d8b8110e31d7756ad7ce8aafd20e074befe6de0
arthur972645 <arthurall.1973@gmail.com>
2024-08-28 08:38:34 -0300

    RF 02 finalizado

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -1,17 +1,40 @@
 import Tarefa from "../models/tarefaModel.js";
 
+//tarefas?page=1&limit=10
+export const getAll = async(request, response) => {
+  const page = parseInt(request.query.page) || 1
+  const limit = parseInt(request.query.limit) || 10
+  const offset = (page - 1) * limit
+
+  try {
+    const tarefas = await Tarefa.findAndCountAll({
+      limit,
+      offset,
+    })
+    
+    const totalPaginas = Math.ceil(tarefas.count / limit)
+    response.status(200).json({
+      totalTarefas: tarefas.count,
+      totalPaginas,
+      paginaAtual: page,
+      itensPorPagina: limit,
+      proximaPagina: totalPaginas === 0 ? null:`http://localhost:3333/tarefas?page=${page+1}`,
+      tarefas: tarefas.rows
+    })
+
+
+  } catch (error){
+    console.error(error)
+    response.status(500).json({message: "Erro ao buscar tarefas"})
+  }
+}
+ 
+
+
 export const create = async (request, response) => {
   const { tarefa, descricao } = request.body;
   const status = "pendente";
   
-  
-  export const getAll = async(request, response) => {
-    try{
-      const tarefas = await Tarefa.findAndCountAll
-    }
-  }
-
-
   if (!tarefa) {
     response.status(400).json({ err: "a tarefa é obrigatória" });
     return;
